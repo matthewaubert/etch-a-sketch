88a6309b016396d726a19139fd4a24da89b7127e
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-25 10:03:41 -0400

    Enable eraser functionality, style buttons

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-startGame();
-
-function startGame() {
-  createGrid();
-  moveSlider();
-  clearBoard();
-  toggleGrid();
-}
+let eraser = false;
+createGrid();
+moveSlider();
+clearBoard();
+toggleEraser();
 
 // function to create grid; input: size (default 16)
 function createGrid(size = 16) {
@@ -25,28 +22,47 @@ function createGrid(size = 16) {
       if (i === size - 1) column.classList.add('border-bottom');
       column.style.width = `${100 / size}%`;
 
-      column.addEventListener('mouseover', () => column.classList.replace('waiting', 'triggered'));
+      changeDrawErase(column);
 
       row.appendChild(column); // add div element to row
     }
     container.appendChild(row); // add row div to container
   }
+  toggleGrid();
 }
 
-// add event listener to slider that calls createGrid
-function moveSlider() {
-  // create selector for slider
-  const slider = document.querySelector('#slider');
-  // add event listener to slider that replaces grid size text with slider value
-  slider.addEventListener('input', () => {
-    const gridSize = document.querySelector('#grid-size');
-    gridSize.textContent = `Grid size: ${slider.value} x ${slider.value}`
+// function to toggle draw and erase functionality
+function changeDrawErase(el) {
+  // const eraserBtn = document.querySelector('#eraser-btn');
+  if (eraser) { // if boolean is true
+    el.removeEventListener('mouseover', enableDraw); // remove draw event listener
+    el.addEventListener('mouseover', enableErase); // add eraser draw listener
+  } else { // if boolean is false
+    el.removeEventListener('mouseover', enableErase); // remove eraser listener
+    el.addEventListener('mouseover', enableDraw); // add draw listener
+  }
+
+  function enableDraw() {
+    el.classList.replace('waiting', 'triggered');
+  }
+  function enableErase() {
+    el.classList.replace('triggered', 'waiting');
+  }
+}
+
+// add event listener to eraser that toggles the eraser
+function toggleEraser() {
+  const eraserBtn = document.querySelector('#eraser-btn');
+  eraserBtn.addEventListener('click', e => {
+    eraser = (!eraser); // set eraser to opposite value
+    e.target.classList.toggle('enabled-btn');
+
+    const columns = document.querySelectorAll('.column') // select all column divs
+    columns.forEach(changeDrawErase); // toggle draw/erase functionality
   });
-  // add event listener to slider that calls createGrid with input of slider value
-  slider.addEventListener('click', () => createGrid(slider.value));
 }
 
-// function that clears board
+// add event listener to 'Clear' that clears board
 function clearBoard() {
   // add event listener to clear button
   const clearBtn = document.querySelector('#clear-btn');
@@ -58,7 +74,7 @@ function clearBoard() {
   });
 }
 
-// function to toggle grid lines
+// add event listener to 'Clear' to 'Toggle Grid Lines' that toggles grid lines
 function toggleGrid() {
   // select all divs with borders applied
   const topLeft = document.querySelectorAll('.border-top-left');
@@ -72,4 +88,17 @@ function toggleGrid() {
     right.forEach(div => div.classList.toggle('border-right'));
     bottom.forEach(div => div.classList.toggle('border-bottom'));
   })
+}
+
+// add event listener to slider that calls createGrid
+function moveSlider() {
+  // create selector for slider
+  const slider = document.querySelector('#slider');
+  // add event listener to slider that replaces grid size text with slider value
+  slider.addEventListener('input', () => {
+    const gridSize = document.querySelector('#grid-size');
+    gridSize.textContent = `Grid size: ${slider.value} x ${slider.value}`;
+  });
+  // add event listener to slider that calls createGrid with input of slider value
+  slider.addEventListener('click', () => createGrid(slider.value));
 }
