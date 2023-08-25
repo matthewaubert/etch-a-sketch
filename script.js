e66fea8e6fb34372f79189f49cee16142fc06b28
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-08-25 12:21:53 -0400

    Enable Rainbow Mode functionality, refactor code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let eraser = false;
+let rainbow = false;
 createGrid();
-moveSlider();
-clearBoard();
 toggleEraser();
+toggleRainbow();
+clearBoard();
+moveSlider();
 
 // function to create grid; input: size (default 16)
 function createGrid(size = 16) {
@@ -22,7 +24,7 @@ function createGrid(size = 16) {
       if (i === size - 1) column.classList.add('border-bottom');
       column.style.width = `${100 / size}%`;
 
-      changeDrawErase(column);
+      changeAction(column);
 
       row.appendChild(column); // add div element to row
     }
@@ -31,50 +33,98 @@ function createGrid(size = 16) {
   toggleGrid();
 }
 
-// function to toggle draw and erase functionality
-function changeDrawErase(el) {
-  // const eraserBtn = document.querySelector('#eraser-btn');
-  if (eraser) { // if boolean is true
+// function to toggle draw, erase, and rainbow functionality
+function changeAction(el) {
+  if (eraser) { // if eraser is true
     el.removeEventListener('mouseover', enableDraw); // remove draw event listener
-    el.addEventListener('mouseover', enableErase); // add eraser draw listener
-  } else { // if boolean is false
+    el.removeEventListener('mouseover', enableRainbow); // remove rainbow event listener
+    el.addEventListener('mouseover', enableErase); // add eraser listener
+  } else if(rainbow) { // if rainbow is true
+    el.removeEventListener('mouseover', enableDraw); // remove draw event listener
     el.removeEventListener('mouseover', enableErase); // remove eraser listener
+    el.addEventListener('mouseover', enableRainbow); // add rainbow listener
+  } else { // if both eraser and rainbow are false
+    el.removeEventListener('mouseover', enableErase); // remove eraser listener
+    el.removeEventListener('mouseover', enableRainbow); // remove rainbow event listener
     el.addEventListener('mouseover', enableDraw); // add draw listener
   }
 
   function enableDraw() {
     el.classList.replace('waiting', 'triggered');
+    el.style.backgroundColor = 'black'; // change background to black
   }
   function enableErase() {
     el.classList.replace('triggered', 'waiting');
+    el.style.backgroundColor = ''; // remove background color
+  }
+  function enableRainbow() {
+    el.classList.replace('waiting', 'triggered');
+
+    // change background color to random color
+    let hexValue = selectRandomHexValue();
+    el.style.backgroundColor = hexValue;
   }
 }
 
-// add event listener to eraser that toggles the eraser
+// add event listener to Eraser btn that toggles the eraser
 function toggleEraser() {
   const eraserBtn = document.querySelector('#eraser-btn');
   eraserBtn.addEventListener('click', e => {
     eraser = (!eraser); // set eraser to opposite value
-    e.target.classList.toggle('enabled-btn');
+    rainbow = false; // set rainbow to false
+    e.target.classList.toggle('enabled-btn'); // toggle button btw normal and blue
 
-    const columns = document.querySelectorAll('.column') // select all column divs
-    columns.forEach(changeDrawErase); // toggle draw/erase functionality
+    // remove blue from rainbow button
+    const rainbowBtn = document.querySelector('#rainbow-btn');
+    rainbowBtn.classList.remove('enabled-btn');
+
+    const columns = document.querySelectorAll('.column'); // select all column divs
+    columns.forEach(changeAction); // toggle draw/erase functionality
   });
 }
 
-// add event listener to 'Clear' that clears board
+// function add event listener to Rainbow Mode btn that toggles rainbow mode
+function toggleRainbow() {
+  const rainbowBtn = document.querySelector('#rainbow-btn');
+  rainbowBtn.addEventListener('click', e => {
+    rainbow = (!rainbow); // set rainbow to opposite value
+    eraser = false; // set eraser to false
+    e.target.classList.toggle('enabled-btn'); // toggle button btw normal and blue
+
+    // remove blue from rainbow button
+    const eraserBtn = document.querySelector('#eraser-btn');
+    eraserBtn.classList.remove('enabled-btn');
+
+    const columns = document.querySelectorAll('.column'); // select all column divs
+    columns.forEach(changeAction);
+  });
+}
+
+// function to select random hexadecimal value
+function selectRandomHexValue() {
+  // select random number from 0 to 256^3 (non-inclusive)
+  let num = Math.floor(Math.random() * (16777216));
+  return '#' + num.toString(16); // return number converted to hex
+}
+
+// add event listener to Clear btn that clears board
 function clearBoard() {
   // add event listener to clear button
   const clearBtn = document.querySelector('#clear-btn');
-  clearBtn.addEventListener('click', () => {
+  clearBtn.addEventListener('click', removeColor);
+
+  function removeColor() {
     // select all triggered divs
     const triggered = document.querySelectorAll('.triggered');
-    // iterate over divs, replace triggered class with waiting class
-    triggered.forEach(div => div.classList.replace('triggered', 'waiting'));
-  });
+    // iterate over divs
+    triggered.forEach(div => {
+      div.classList.replace('triggered', 'waiting'); // replace triggered class with waiting class
+      div.style.backgroundColor = ''; // remove background color
+    });
+  }
 }
 
-// add event listener to 'Clear' to 'Toggle Grid Lines' that toggles grid lines
+// add event listener to Toggle Grid Lines btn that toggles grid lines
 function toggleGrid() {
   // select all divs with borders applied
   const topLeft = document.querySelectorAll('.border-top-left');
@@ -83,6 +133,7 @@ function toggleGrid() {
   // add event listener to Toggle Grid Lines button
   const gridBtn = document.querySelector('#grid-btn');
   gridBtn.addEventListener('click', () => {
+    // e.target.classList.toggle('enabled-btn'); // toggle button btw normal and blue
     // iterate over divs, toggling border class
     topLeft.forEach(div => div.classList.toggle('border-top-left'));
     right.forEach(div => div.classList.toggle('border-right'));
